623f235a4bda0d641f13e2aee3c39b13891588c4
Joey Huang <tul32032@temple.edu>
2026-03-22 20:44:45 -0400

    add email password signup/login

--- server/authentication/urls.py
+++ server/authentication/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import GoogleLoginApi, MeApi, Logout
+from .views import GoogleLoginApi, EmailSignupApi, EmailLoginApi, MeApi, Logout
 
 urlpatterns = [
     path("auth/login/google/", GoogleLoginApi.as_view(), name="login-with-google"),
+    path("auth/signup/email/", EmailSignupApi.as_view(), name="signup-with-email"),
+    path("auth/login/email/", EmailLoginApi.as_view(), name="login-with-email"),
     path("auth/me/", MeApi.as_view(), name="me"),
     path("auth/logout/", Logout.as_view(), name="logout"),
 ]
