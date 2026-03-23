de3b5c869074d60666173855de445de3bb646aec
Joey Huang <tul32032@temple.edu>
2026-03-22 20:23:31 -0400

    change docker file

--- frontend/src/constants/constants.ts
+++ frontend/src/constants/constants.ts
@@ -3,8 +3,6 @@ const USE_LOCAL = false;
 
 export const GOOGLE_CLIENT_ID = "524283018158-p47d378627gbsposbe6058a0ltnkd8q3.apps.googleusercontent.com";
 
-export const AI_INFRA_API = "http://10.0.1.5"
-
 export const API_BASE_URL = USE_LOCAL
   ? "http://localhost:8000"
   : "https://capstone-medical-gen-ai-server-524283018158.us-east1.run.app";
