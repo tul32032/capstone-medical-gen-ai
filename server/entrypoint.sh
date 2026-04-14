bbc4a2ae011ac6a80750029109338e443fd500f7
jclark-tun77976 <tun77976@temple.edu>
2026-04-13 23:12:05 -0400

    Fix worker timeout

--- server/entrypoint.sh
+++ server/entrypoint.sh
@@ -5,4 +5,4 @@ uv run python manage.py makemigrations
 uv run python manage.py migrate
 
 echo "Starting server..."
-uv run gunicorn betesbot.wsgi:application --bind 0.0.0.0:8000 --workers 4
+uv run gunicorn betesbot.wsgi:application --bind 0.0.0.0:8000 --workers 4 --timeout 180
